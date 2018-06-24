4d521e4758f38c10c00697549d5709a1d9b3a2f9
Nikola Petrov <nikitvmaniak@gmail.com>
2018-06-24 14:25:08 +0300

    цветове

---
+++ app/EventsColors.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+/*
+ *
+ * Какви да са цветовете на целодневните събития,
+ * в зависимост от типа им
+ *
+ */
+
+class EventsColors
+{
+    const  colors=[
+        'restDay' =>'pink',
+        'nameDay' =>'yellow',
+        'promotion'=>'purple',
+    ];
+}
